409f20a268a868d582a9f66a2632d8012f555f48
Gmowery123 <gmowery@wvup.edu>
2026-01-14 10:01:50 -0500

    fixing Parser

--- ZuulRemake/ZuulRemake/Classes/Parser.cs
+++ ZuulRemake/ZuulRemake/Classes/Parser.cs
@@ -29,20 +29,10 @@ namespace ZuulRemake.Classes
 
             Console.Write("> ");     // print prompt
 
-            inputLine = reader.nextLine();
+            inputLine = Console.ReadLine();
 
-            try (// Find up to two words on the line.
-            Scanner tokenizer = new Scanner(inputLine)) {
-                if (tokenizer.hasNext())
-                {
-                    word1 = tokenizer.next();      // get first word
-                    if (tokenizer.hasNext())
-                    {
-                        word2 = tokenizer.next();      // get second word
-                                                       // note: we just ignore the rest of the input line.
-                    }
-                }
-            }
+            if (!string.IsNullOrWhiteSpace(inputLine)) {
+                string[] words = inputLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
             // Now check whether this word is known. If so, create a command
             // with it. If not, create a "null" command (for unknown command).
             return new Command(commands.getCommandWord(word1), word2);
